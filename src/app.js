7f504bbbfe2b3690e76dc70d6b21be46e32bf075
muazzam51214 <syedmuazzamabbas2004@gmail.com>
2024-09-07 01:32:23 +0500

    add style to main listing page also configured ejs-mate and added static assets

--- src/app.js
+++ src/app.js
@@ -1,17 +1,19 @@
 import express from "express";
-import methodOverRide from "method-override"
-import path from 'path';
-import { fileURLToPath } from 'url';
+import methodOverRide from "method-override";
+import ejsMate from "ejs-mate";
+import path from "path";
+import { fileURLToPath } from "url";
 const app = express();
 // Setting View Engine
 app.set("view engine", "ejs");
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.set("views", path.join(__dirname, "views"));
+app.engine("ejs", ejsMate);
 
-app.use(methodOverRide("_method"))
+app.use(methodOverRide("_method"));
 app.use(express.static("public"));
-app.use(express.json({limit: "16kb"}));
+app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
 // Routes Import
@@ -22,4 +24,4 @@ import listingRouter from "./routes/listing.routes.js";
 app.use("/users", userRouter);
 app.use("/listing", listingRouter);
 
-export { app }
+export { app };
