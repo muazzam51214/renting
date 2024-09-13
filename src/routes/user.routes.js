7bbe9686ed068391666133c2c11be741262adae7
muazzam51214 <syedmuazzamabbas2004@gmail.com>
2024-09-14 00:36:52 +0500

    showing all listing page in / route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -7,9 +7,10 @@ import {
   logoutUser,
 } from "../controllers/user.controllers.js";
 import { isLoggedIn } from "../middlewares/isLoggedIn.middleware.js";
+import { allListings } from "../controllers/listing.controllers.js";
 
 const router = Router();
-
+router.route("").get(allListings);
 router.route("/signup").get(signupUser).post(registerUser);
 
 router.route("/login").get(loginUserForm).post(loginUserController);
